ad0c9507b5c906e5519043e97bfde0908a50b470
Ryan <ryepdx@gmail.com>
2011-06-22 11:25:23 -0700

    Initial commit.

---
+++ appraise.pl
@@ -0,0 +1,26 @@
+% appraised holds all the values the computer figured out for itself.
+:- dynamic appraised/3.
+
+% value holds all the values we've entered by hand.
+:- dynamic value/3.
+
+% Some quick nonsense assertions to get you started. Feel free to delete these.
+value(banana, 1 rdiv 4, bitcoin).
+value(bitcoin, 1 rdiv 30, namecoin).
+value(apple, 3 rdiv 4, banana).
+value(orange, 1 rdiv 5, apple).
+
+% Naive pathfinding algorithm.
+% TODO: Keep track of route taken.
+appraise(Thing1, Price, Thing2) :- value(Thing1, Price, Thing2).
+appraise(Thing1, Price, Thing2) :- value(Thing2, BackPrice, Thing1), Price is 1 rdiv BackPrice, assert(appraised(Thing1, Price, Thing2)).
+appraise(Thing1, Price, Thing2) :- appraised(Thing1, Price, Thing2).
+appraise(Thing1, Price, Thing2) :- appraised(Thing2, BackPrice, Thing1), Price is 1 rdiv BackPrice, assert(appraised(Thing1, Price, Thing2)).
+appraise(Thing1, Price, Thing2) :- appraise(Thing1, Price1, Thing3), appraise(Thing2, Price2, Thing3), Price is Price1 rdiv Price2, assert(appraised(Thing1, Price, Thing2)).
+
+appraise_float(Thing1, Price, Thing2) :- appraise(Thing1, RPrice, Thing2), Price is float(RPrice).
+
+% Simple user-friendly function to find cost of n things in terms of
+% something else.
+find_cost(Quantity, Thing1, Cost, Thing2) :- appraise(Thing1, Price, Thing2), Cost is Quantity*Price.
+
